5f9b27637c5dd8e6a597c21f36a98b327370a9e6
Kevin Blanchard <blanchardkevinj@gmail.com>
2024-06-01 16:05:42 -0400

    yes use

--- src/GoonWorld/main.cpp
+++ src/GoonWorld/main.cpp
@@ -1,6 +1,9 @@
 #include <GoonEngine/SdlSurface.h>
 #include <GoonEngine/content/bgm.h>
+#include <GoonEngine/content/textv2.h>
 #include <GoonEngine/content/content.h>
+#include <GoonEngine/clock.h>
+#include <GoonEngine/utils.h>
 #include <GoonEngine/debug.h>
 #include <GoonEngine/game.h>
 
@@ -14,23 +17,19 @@ void Update(double deltatime) {}
 void Draw() {
 	geDrawRect(&thingLoc, &color);
 	geRichTextDraw(thing);
+	geUtilsDrawFps();
 }
 
 void initBgm() {
-	// This should be done inside of the engine
-	geInitializeBgmContentType();
 	bgm = geBgmNew("1-2", 8.727f, 0.0f);
 }
 
 int main() {
-	SetLogLevel(Log_LDebug);
+	// SetLogLevel(Log_LDebug);
 	geInitializeEngine();
 	geGameSetUpdateFunc(Update);
 	geGameSetDrawFunc(Draw);
 	initBgm();
-	geInitializeFontContentType();
-	geInitializeTextv2ContentType();
-	geInitializeImageContentType();
 	auto size = Point{400, 150};
 	thing = geRichTextNew("Hello world!", "BitPotion", 32, &size);
 	geRichTextSetDrawRect(thing, &thingLoc);
@@ -40,6 +39,6 @@ int main() {
 	}
 	gePlayLoop();
 	geRichTextFree(thing);
-	geUnloadAllContent();
+	// geUnloadAllContent();
 	return 0;
 }
