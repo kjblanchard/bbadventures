012cb8a942a1004afb9e70b89fcfbcc3930cfd8a
Kevin Blanchard <blanchardkevinj@gmail.com>
2024-06-01 20:28:25 -0400

    yes

--- src/GoonWorld/main.cpp
+++ src/GoonWorld/main.cpp
@@ -1,23 +1,20 @@
-#include <GoonEngine/SdlSurface.h>
+#include <GoonEngine/primitives/color.h>
 #include <GoonEngine/content/bgm.h>
-#include <GoonEngine/content/textv2.h>
+#include <GoonEngine/content/text.h>
 #include <GoonEngine/content/content.h>
-#include <GoonEngine/clock.h>
 #include <GoonEngine/utils.h>
 #include <GoonEngine/debug.h>
 #include <GoonEngine/game.h>
 
-static geRichText *thing = nullptr;
+static geText *thing = nullptr;
 static geBgm *bgm = nullptr;
 static geRectangle thingLoc = {100, 100, 400, 150};
-static geColor color = geColor{255, 255, 255, 255};
 
 void Update(double deltatime) {}
 
 void Draw() {
-	geDrawRect(&thingLoc, &color);
-	geRichTextDraw(thing);
-	geUtilsDrawFps();
+	// geDrawRect(&thingLoc, &color);
+	geTextDraw(thing);
 }
 
 void initBgm() {
@@ -30,15 +27,14 @@ int main() {
 	geGameSetUpdateFunc(Update);
 	geGameSetDrawFunc(Draw);
 	initBgm();
-	auto size = Point{400, 150};
-	thing = geRichTextNew("Hello world!", "BitPotion", 32, &size);
-	geRichTextSetDrawRect(thing, &thingLoc);
+	thing = geTextNew("Hello world!", "BitPotion", 32);
+	geTextSetDrawRect(thing, &thingLoc);
 	geLoadAllContent();
 	if (bgm) {
 		geBgmPlay(bgm, 1.0, -1);
 	}
 	gePlayLoop();
-	geRichTextFree(thing);
+	geTextFree(thing);
 	// geUnloadAllContent();
 	return 0;
 }
